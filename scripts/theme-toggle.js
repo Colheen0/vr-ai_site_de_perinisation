e8948e449179951c6457a64b6d6adc57bc1fc4f6
antoine <antoineswg01@gmail.com>
2026-05-20 10:36:46 +0200

    updated theme selection logic

--- scripts/theme-toggle.js
+++ scripts/theme-toggle.js
@@ -1,7 +1,16 @@
 const buttonToggle = document.querySelector('[data-theme-toggle]');
 const htmlElement = document.documentElement;
+
+const savedTheme = localStorage.getItem('theme');
+const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const fileTheme = htmlElement.getAttribute('data-theme');
+
+const initialTheme = savedTheme ?? systemTheme ?? fileTheme;
+htmlElement.setAttribute('data-theme', initialTheme);
+
 buttonToggle.addEventListener('click', () => {
-const currentTheme = htmlElement.getAttribute('data-theme');
-const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-htmlElement.setAttribute('data-theme', newTheme);
+    const currentTheme = htmlElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    htmlElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
 });
